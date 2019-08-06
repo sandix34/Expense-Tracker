f77bc58413d03cc17d792223df0463da1fdd6279
sandrine <sandrinezocca@gmail.com>
2019-08-06 16:27:24 +0200

    Récupération de la configuration d'une base Firestore

---
+++ src/store/env-model.js
@@ -0,0 +1,13 @@
+//env.js
+
+// Your web app's Firebase configuration
+
+export const firebaseConfig = {
+    apiKey: "---------",
+    authDomain: "--------",
+    databaseURL: "---------",
+    projectId: "--------",
+    storageBucket: "-----------",
+    messagingSenderId: "-----------",
+    appId: "----------"
+  };
